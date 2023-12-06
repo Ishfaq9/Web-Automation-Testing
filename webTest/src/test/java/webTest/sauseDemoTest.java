03c30e960f5d41e1efd5ca9e31a8cac4813d9c35
Ishfaq <Ishfaq@192.168.1.3>
2023-12-06 19:54:43 +0600

    Login

---
+++ webTest/src/test/java/webTest/sauseDemoTest.java
@@ -0,0 +1,33 @@
+package webTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class sauseDemoTest {
+	
+	WebDriver driver;
+	@BeforeTest
+	public void init()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void logIn()
+	{
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+	}
+	
+	
+
+}
